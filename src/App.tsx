a518fd45648597c0cbcfd2cc71f709f5ec707e92
hakeemullahjan <hakeemullah.yousufzai@gmail.com>
2022-06-04 01:22:23 +0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import abi from "./abi.json";
+import Web3 from "web3";
+
+const CONTRACT_ADDRESS = "0xF22392D227B4Cb26de3A99bbd90B4E5D92f6C455";
 
 function App() {
+  const [currentAccount, setCurrentAccount] = useState(null);
+  const [contract, setContract] = useState<any>(null);
+
+  const connectWalletAction = async () => {
+    try {
+      const { ethereum }: any = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      var web3 = new Web3(ethereum);
+      var contract = new web3.eth.Contract(abi as any, CONTRACT_ADDRESS);
+
+      setContract(contract);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const mint = async () => {
+    if (contract && currentAccount) {
+      await contract.methods
+        .mint()
+        .send({
+          from: currentAccount,
+          gasLimit: 1000000,
+        })
+        .on("transactionHash", (hash: any) => {
+          // hash of tx
+          console.log("hash", hash);
+        })
+        .on("confirmation", function (confirmationNumber: any, receipt: any) {
+          if (confirmationNumber === 2) {
+            // tx confirmed
+            console.log("confirmation", confirmationNumber, receipt);
+          }
+        })
+        .on("error", function (err: any) {
+          console.log("err", err);
+        });
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {currentAccount ? (
+        <h2>{currentAccount}</h2>
+      ) : (
+        <button onClick={connectWalletAction}>Connect Wallet</button>
+      )}
+      <button onClick={mint}>Mint</button>
     </div>
   );
 }
